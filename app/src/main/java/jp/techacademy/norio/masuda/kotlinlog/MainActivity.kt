3c883b8c4c2a7bb756f8c571f1ac0c03f2618cf0
masudanorio <akamikechoro@gmail.com>
2021-01-08 00:02:25 +0900

    humanインスタンスを2つ作成

--- app/src/main/java/jp/techacademy/norio/masuda/kotlinlog/MainActivity.kt
+++ app/src/main/java/jp/techacademy/norio/masuda/kotlinlog/MainActivity.kt
@@ -3,6 +3,7 @@ package jp.techacademy.norio.masuda.kotlinlog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log   // ここを追加
+import jp.techacademy.norio.masuda.kotlinlog.Human as Human1
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,11 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val human = Human("のりお", 34, hobby = "読書")      // 名前を〜〜、年齢〜〜歳で、Humanのインスタンスを作る
+        val human = Human("のりお", 34)      // 名前を〜〜、年齢〜〜歳で、Humanのインスタンスを作る
 
         human.say()
 
+        val human = Human(hobby = "読書")      // 趣味は〜〜で、Humanのインスタンスを作る
+
         human.think()
+
 
 
     }
